48e555c0b2eaf2642dec7d2a6f11938a9de47a13
Ales Tomecek <TomecekAles@seznam.cz>
2012-11-29 22:19:01 +0100

    added zooming on initial position lock

--- src/net/solajpafistoj/tag/client/TagsMapActivity.java
+++ src/net/solajpafistoj/tag/client/TagsMapActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.OverlayItem;
@@ -17,6 +18,9 @@ public class TagsMapActivity extends MapActivity {
 
 	EnableDraw enableDraw;
 	
+	//for controlling zooming, location etc...
+	private MapController mapController;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,9 @@ public class TagsMapActivity extends MapActivity {
         
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
-                
+
+        mapController = mapView.getController();
+        
         
         //test overlay of items 
         Drawable drawable =new TagDrawable();// this.getResources().getDrawable(R.drawable.androidmarker);
@@ -42,9 +48,17 @@ public class TagsMapActivity extends MapActivity {
         
         //current location overlay
         
-        MyLocationOverlay mylocationOverlay = new MyLocationOverlay(this, mapView);
+        final MyLocationOverlay mylocationOverlay = new MyLocationOverlay(this, mapView);
         mylocationOverlay.enableMyLocation();
         mapView.getOverlays().add(mylocationOverlay);
+        
+        //as soon as we get fix, move map to that position
+        mylocationOverlay.runOnFirstFix( new Runnable(){
+        									public void run() {
+        										mapController.animateTo( mylocationOverlay.getMyLocation() );		
+        									}
+        								});
+        
         
         
 
