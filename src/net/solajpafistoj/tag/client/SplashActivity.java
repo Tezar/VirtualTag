4727729891b3fc54db79a3a8d4de1fad76630bdf
Ales Tomecek <TomecekAles@seznam.cz>
2012-12-08 14:53:03 +0100

    reduced splash to 1sec

--- src/net/solajpafistoj/tag/client/SplashActivity.java
+++ src/net/solajpafistoj/tag/client/SplashActivity.java
@@ -21,12 +21,12 @@ public class SplashActivity extends Activity{
  	
         setContentView(R.layout.activity_splash);
 	
-        // Creates a thread that lets the splash run for 5 seconds
+        // Creates a thread that lets the splash run for 1 second
 	    Thread logoTimer = new Thread(){
 	    	public void run(){
 	    		try{
 	    			int logoTimer = 0;
-	    			while( logoTimer < 5000 ){
+	    			while( logoTimer < 1000 ){
 	    				sleep(100);
 	    				logoTimer = logoTimer + 100;
 	    			}
