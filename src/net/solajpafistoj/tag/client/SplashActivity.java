8d0f8198c36006e613e155ba3838bd67f8def33b
Line Reinsaas <line.beathe@gmail.com>
2012-12-12 21:32:08 +0100

    changing splash, and locking it to portrait

--- src/net/solajpafistoj/tag/client/SplashActivity.java
+++ src/net/solajpafistoj/tag/client/SplashActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,7 +18,11 @@ public class SplashActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-       
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
  	
         setContentView(R.layout.activity_splash);
 	
