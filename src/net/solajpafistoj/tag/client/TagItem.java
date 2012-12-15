012e7f013d9a76b9ce8203891d869b24c93bb1bc
Ales Tomecek <TomecekAles@seznam.cz>
2012-12-15 21:48:41 +0100

    added content to tag item

--- src/net/solajpafistoj/tag/client/TagItem.java
+++ src/net/solajpafistoj/tag/client/TagItem.java
@@ -1,7 +1,10 @@
 package net.solajpafistoj.tag.client;
 
+import java.util.ArrayList;
+
 import com.google.android.maps.GeoPoint;
 
 public class TagItem {
 	public GeoPoint location;
+	public ArrayList<ArrayList<Float>> strokes;
 }
