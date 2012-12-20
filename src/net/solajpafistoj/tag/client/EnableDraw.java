60c049bb71cad49577a8bea8b20959fcaa5c2413
Line Reinsaas <line.beathe@gmail.com>
2012-12-20 20:44:36 +0100

    full circle through HSV wheel every 6th hour

--- src/net/solajpafistoj/tag/client/EnableDraw.java
+++ src/net/solajpafistoj/tag/client/EnableDraw.java
@@ -33,7 +33,7 @@ public class EnableDraw extends SurfaceView implements OnTouchListener, Runnable
     	mPaint = new Paint();
         mPaint.setDither(true); // decieves the human eye
         
-        float[] hsv = {(float) ((System.currentTimeMillis() / 1000f)%360),1.f, 1.f};
+        float[] hsv = {(float) ((System.currentTimeMillis() / 1000f)%(3600*6))/60.f,1.f, 1.f};
         
         currentPaint = Color.HSVToColor(hsv);
         mPaint.setColor(currentPaint);
